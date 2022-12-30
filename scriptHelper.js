f3d0b037f2b0e694dbc7a9316fcfd6b3d6aaeada
KitPyles <pyles.kit@gmail.com>
2022-12-29 18:55:19 -0600

    Completed validateInput(). Partial formSubmission().

--- scriptHelper.js
+++ scriptHelper.js
@@ -17,17 +17,52 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
 }
 
 function validateInput(testInput) {
-   
+   if (testInput === "") {
+           return "Empty";
+       } else if (isNaN(testInput)) {
+           return "Not a Number";
+       } else {
+           return "Is a Number";
+       };
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+   list.style.visibility = "hidden";
+
+       const launchStatus = document.getElementById("launchStatus");
+       const faultyItems = document.getElementById("faultyItems");
+       const pilotStatus = document.getElementById("pilotStatus");
+       const copilotStatus = document.getElementById("copilotStatus");
+       const fuelStatus = document.getElementById("fuelStatus");
+       const cargoStatus = document.getElementById("cargoStatus");
+
+       if (validateInput(pilot) === "Not a Number" && validateInput(copilot) === "Not a Number" && validateInput(fuelLevel) === "Is a Number" && validateInput(cargoLevel) === "Is a Number") {
+           list.style.visibility = "visible";
+           pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
+           copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
+           if (fuelLevel >= 10000 && cargoLevel <= 10000) {
+               launchStatus.innerHTML = "Shuttle is Ready for Launch";
+               launchStatus.style.color = "rgb(65, 159, 106)";
+           } else {
+               launchStatus.innerHTML = "Shuttle Not Ready for Launch";
+               launchStatus.style.color = "rgb(199, 37, 78)";
+               // list.style.visibility = "visible";
+           }
+
+           if (fuelLevel < 10000) {
+               fuelStatus.innerHTML = "Fuel level too low for launch";
+           }
+
+           if (cargoLevel > 10000) {
+               cargoStatus.innerHTML = "Cargo mass too heavy for launch";
+           }
+       }
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
+    planetsReturned = await fetch().then(function (response) {
         });
 
     return planetsReturned;
