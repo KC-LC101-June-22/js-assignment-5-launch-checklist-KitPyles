aabb8d57a8f306a976d3a74fbc0bc3d6bc8780cb
KitPyles <pyles.kit@gmail.com>
2022-12-29 19:19:18 -0600

    Final migration of completed code to new GH account.

--- script.js
+++ script.js
@@ -9,9 +9,10 @@ window.addEventListener("load", function () {
     const copilotName = document.querySelector("input[name=copilotName]");
     const fuelLevel = document.querySelector("input[name=fuelLevel]");
     const cargoMass = document.querySelector("input[name=cargoMass]");
+
     let response = {};
 
-     list.style.visibility = "hidden";
+    list.style.visibility = "hidden";
 
     form.addEventListener("submit", function(event) {
         if (pilotName.value === "" || copilotName.value === "" || fuelLevel.value === "" || cargoMass.value === "") {
@@ -23,21 +24,23 @@ window.addEventListener("load", function () {
                 fuelLevel: fuelLevel.value,
                 cargoLevel: cargoMass.value
             };
-                        formSubmission(window.document,list,response.pilot,response.copilot,response.fuelLevel,response.cargoLevel);
+            formSubmission(window.document,list,response.pilot,response.copilot,response.fuelLevel,response.cargoLevel);
         }
-            event.preventDefault();
-        });
+        event.preventDefault();
+    });
 
+    let listedPlanets;
+    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
-        listedPlanetsResponse.then(function (result) {
-            listedPlanets = result;
-//            console.log(listedPlanets);
-        }).then(function () {
-            // console.log(listedPlanets);
-            // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
-            let picked = pickPlanet(listedPlanets);
-            console.log(picked);
-            addDestinationInfo(window.document,picked.name, picked.diameter,picked.star,picked.distance,picked.moons,picked.image);
-        })
-   
+    listedPlanetsResponse.then(function (result) {
+        listedPlanets = result;
+        // console.log(listedPlanets);
+    }).then(function () {
+        // console.log(listedPlanets);
+        // Below this comment call the appropriate helper functions to pick a planet from the list of planets and add that information to your destination.
+        let picked = pickPlanet(listedPlanets);
+        console.log(picked);
+        addDestinationInfo(window.document,picked.name, picked.diameter,picked.star,picked.distance,picked.moons,picked.image);
+    })
+
 });
